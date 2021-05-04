31e9f92349add0d72c2136d4084546bea02dfed8
nikol411@hotmail.com <nikol411@hotmail.com>
2021-05-03 21:35:29 -0400

    html and css styling updates

--- script.js
+++ script.js
@@ -9,19 +9,18 @@ currentDate();
 $(document).ready(function () {
     // saveBtn click listener 
     $(".saveBtn").on("click", function () {
-        // Get nearby values of the description in JQuery
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
 
-        // Save text in local storage
+        // Save to local storage
         localStorage.setItem(time, text);
     })
    
+    // time tracker
     function timeTracker() {
-        //get current number of hours.
         var timeNow = moment().hour();
 
-        // time block loop
+        // time blocks loop
         $(".time-block").each(function () {
             var blockTime = parseInt($(this).attr("id").split("hour")[1]);
 
@@ -44,7 +43,7 @@ $(document).ready(function () {
         })
     }
 
-    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    
     $("#hour9 .description").val(localStorage.getItem("hour9"));
     $("#hour10 .description").val(localStorage.getItem("hour10"));
     $("#hour11 .description").val(localStorage.getItem("hour11"));
